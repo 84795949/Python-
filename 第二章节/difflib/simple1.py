64cf82ac2400fbc34bfcaa2787eb11db5ca08ef9
root <84795949@qq.com>
2017-12-29 11:27:46 +0800

    Differ()方法对比文件差异,新添HtmlDiff方法输出HTML页面

--- 第二章节/difflib/simple1.py
+++ 第二章节/difflib/simple1.py
@@ -20,6 +20,11 @@ hello3c
 
 text2_lines = text2.splitlines()
 
-d = difflib.Differ()
-diff = d.compare(text1_lines, text2_lines)
-print '\n'.join(list(diff))
+#显示差异
+#d = difflib.Differ()
+#diff = d.compare(text1_lines, text2_lines)
+#print '\n'.join(list(diff))
+
+#生成美观的HTML展示出来,直接运行结果>diff.html，可以用浏览器打开
+d = difflib.HtmlDiff()
+print d.make_file(text1_lines, text2_lines)
